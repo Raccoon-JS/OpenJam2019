0f50e7b1e450f43dd762f0a7838abef8459b8bbd
Raccoon-JS <raccoon_js@protonmail.com>
2019-09-28 13:30:30 -0500

    updated

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -5,4 +5,7 @@ extends Node
 func _ready():
 	$background.color = Color(colorPalette.colors[0])
 	$image.modulate = Color(colorPalette.colors[4])
+	$title/panel.color = Color(colorPalette.colors[2])
+	$title/words.modulate = Color(colorPalette.colors[3])
+	$AnimationPlayer.play("title")
 	pass # Replace with function body.
